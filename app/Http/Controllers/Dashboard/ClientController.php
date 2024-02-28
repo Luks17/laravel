4ce8101815bd7b037eb314fd62ebdaf0fd9ef4c9
Luks17 <lucasthidavort@gmail.com>
2024-02-28 09:24:46 -0400

    Clients

---
+++ app/Http/Controllers/Dashboard/ClientController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\Client;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ClientController extends Controller
+{
+    public readonly Client $client;
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $clients = $this->client->all();
+
+        return Inertia::render("Dashboard/Clients", [
+            "clients" => $clients
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Dashboard\ClientController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -18,13 +20,8 @@ Route::get('/', function () {
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard/Index');
-    })->name('dashboard');
-
-    Route::get("/dashboard/clients", function () {
-        return Inertia::render("Dashboard/Clients");
-    })->name("clients");
+    Route::get('/dashboard',  [DashboardController::class, "index"])->name("dashboard");
+    Route::get("/dashboard/clients", [ClientController::class, "index"])->name("dashboard.clients");
     
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
